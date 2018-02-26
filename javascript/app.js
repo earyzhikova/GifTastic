1863d2b126f5087e8af27a2d9740de1ca31d95c7
earyzhikova <elenaryzh55@gmail.com>
2018-02-26 15:41:04 -0500

    GifTastic

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 
-// // initial array of celebrities
 var celebrities = ["Bill Murray", "Adam Sandler", "Angelina Jolie", "Tom Hanks", "Rashida Jones", "Nicolas Cage", "Jackie Chan", "Silvester Stallone", "Emma Stone", "Jackie Chan"];
+// // initial array of celebrities
 
 //Page Ready Function
 $(function(){
@@ -11,20 +11,19 @@ $(function(){
 function renderButtons() {
 
     // Deleting the buttons prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
-    $("#button").empty();
+       $("#button").empty();
 
-    // Looping through the array of movies
+    // Looping through the array of names
     for (var i = 0; i < celebrities.length; i++) {
 
-      // Then dynamically generating buttons for each movie in the array
+      // Then dynamically generating buttons for each name in the array
       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
       var a = $("<button>");
       // Adding a class of movie to our button
       a.addClass("images");
       // Adding a data-attribute
       a.attr("data-name", celebrities[i]);
-      a.attr("id","celebrity-button")
+      a.attr("id","data-celebtrities")
       // Providing the initial button text
       a.text(celebrities[i]);
       // Adding the button to the buttons-view div
@@ -36,20 +35,24 @@ function renderButtons() {
 
 // // URL   https://giphy.com/categories/celebrities?api_key=8BRi8Mur7x5UXHkxRh2ZfTVqOjpspRfl
 
-function displayInfo() {
+function displayInfo(celebrityName) {
 
-var gueryURL = "http://api.giphy.com/v1/gifs/search?q="+x+"&api_key=8BRi8Mur7x5UXHkxRh2ZfTVqOjpspRfl&limit=10";
-$.ajax({url: queryURL, method: "GET"})
-.done(function(response) {
-	console.log(response);
-});
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+celebrityName+"&api_key=8BRi8Mur7x5UXHkxRh2ZfTVqOjpspRfl&limit=10";
+
+	$.ajax({url: queryURL, method: "GET"}).done(function(response) {
+		console.log(response);
+	});
 }
 
-$('#celebrity-button').on("click", function(event) {
+$(document).on('click', '#data-celebtrities', function(){
 	console.log("button clicked");
 	// which button to click
 	var x = $(this).attr("data-name");
 	console.log(x);
+	displayInfo(x);
 })
+
+
+
 
 // // http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=YOUR_API_KEY
